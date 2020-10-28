2f130c6556345247d90ab9006f26c551b6c22bef
shota.kirameki <ozawa@ualberta.ca>
2020-10-28 09:12:18 +0000

    for heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,5 @@
 class UsersController < ApplicationController
   
-  def index
-  end
-
-  def show
-  end
-
   def new
     @user = User.new
   end
@@ -15,7 +9,7 @@ class UsersController < ApplicationController
 
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to root_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
