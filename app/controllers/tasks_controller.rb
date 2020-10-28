fdb5012ecb6b0ea8938664167801a30e2cbf1394
shota.kirameki <ozawa@ualberta.ca>
2020-10-28 02:19:10 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -49,10 +49,17 @@ class TasksController < ApplicationController
   private
   
   def set_task
-    @task = Task.find_by(id: params[:id])
+    @task = current_user.tasks.find_by(id: params[:id])
     if @task
     else
       redirect_to tasks_url
+    end
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
     end
   end
   
