a60fb8670cfe70da738b1c5da6e9c41f9683034f
shota.kirameki <ozawa@ualberta.ca>
2020-10-28 08:33:11 +0000

    for heroku

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,6 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update,]
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
   
   def index
       @tasks = current_user.tasks
@@ -52,13 +51,6 @@ class TasksController < ApplicationController
     if @task
     else
       redirect_to tasks_url
-    end
-  end
-  
-  def correct_user
-    @task = current_user.tasks.find_by(id: params[:id])
-    unless @task
-      redirect_to root_url
     end
   end
   
